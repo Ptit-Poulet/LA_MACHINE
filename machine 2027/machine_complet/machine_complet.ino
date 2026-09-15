c4ee00a42723f3b32cd2862e1f82bce8a7a67a90
Sam <sambubulle@gmail.com>
2026-09-15 15:39:04 -0400

    Commencement prog

---
+++ machine 2027/machine_complet/machine_complet.ino
@@ -0,0 +1,29 @@
+/*
+Fichier pour tout réunir les différentes foncitonnalités que le robot devra faire.
+- Rouler avec mecanums
+  - Avant
+  - Arrière
+  - Gauche
+  - Droite
+  - Tourne gauche
+  - Tourne droite
+- Fonctionnement du bras
+  - Fermeture/Ouverture Pince
+  - Poignet (peut faire sans ?) 
+  - Coude
+  - Épaule
+  - Rotation base (où l'épaule)
+  - Slider 
+*/
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
